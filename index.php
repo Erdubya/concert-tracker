c8c9e24a749de765d1886c6bf9617586ae66755e
Erik Wilson <erw2214@rit.edu>
2017-04-13 23:34:23 -0400

    Add install check on index.

--- index.php
+++ index.php
@@ -4,11 +4,18 @@
  * Date: 13-Apr-17
  * Time: 21:23
  */
+// check if installed
+if (!file_exists('config.php')) {
+    die("Please run the <a href='install.php'>install script</a> set up Concert Tracker.");
+}
+
 require_once "config.php";
+
 session_start();
 $dbh = db_connect() or die(ERR_MSG);
 
 ob_start();
+echo "Page is loaded";
 ?>
 <!--PAGE GOES HERE-->
 <?php
