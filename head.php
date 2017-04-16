52f2f414fc1690d4ae00b0d08a7b606900aa4cca
Erik Wilson <erw2214@rit.edu>
2017-04-16 00:12:36 -0400

    Move HTML head to separate file

---
+++ head.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * User: Erik Wilson
+ * Date: 16-Apr-17
+ * Time: 00:00
+ * 
+ * Include file to get rid of all that nasty suff clutering the head of the html files.
+ */
+?>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- jQueryUI -->
+    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+	
+	<!-- Use this variable for extra styles/scripts	-->
+	<? if (isset($extra_includes)) {
+		echo $extra_includes;
+	}?>
+
+	<!-- Set the page title before the HTML section-->
+	<title>
+	<? if (isset($pagetitle)) {
+		echo $pagetitle;
+	} else {
+		echo "Default Title";
+	}?>
+	</title>
+</head>
