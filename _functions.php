b30c59366956a6d29a52fe89f85eea7369cb9475
Erik Wilson <erikwilson@outlook.com>
2018-04-09 22:39:27 -0400

    Build out tables to support multiple artists at concerts

--- _functions.php
+++ _functions.php
@@ -43,6 +43,7 @@ function build_db($dbh, $handler)
         $dbh->exec($tables['user']);
         $dbh->exec($tables['artist']);
         $dbh->exec($tables['concert']);
+        $dbh->exec($tables['concert_artists']);
         $dbh->exec($tables['token']);
 
         return true;
@@ -107,43 +108,51 @@ function mysql_tables()
 function pgsql_tables()
 {
     unset($tables);
-    $tables['user']    = "CREATE TABLE IF NOT EXISTS users(
-                          user_id SERIAL,
-                          email VARCHAR(30) UNIQUE NOT NULL ,
-                          passwd VARCHAR(255) NOT NULL ,
-                          name VARCHAR(50) NOT NULL ,
-                          PRIMARY KEY (user_id)
-                          )";
-    $tables['artist']  = "CREATE TABLE IF NOT EXISTS artist(
-						  artist_id SERIAL ,
-						  user_id INT NOT NULL ,
-						  name VARCHAR(50) NOT NULL ,
-						  genre VARCHAR(50) NULL , 
-						  country VARCHAR(50) NULL ,
-						  PRIMARY KEY (artist_id),
-						  FOREIGN KEY (user_id) REFERENCES users(user_id),
-						  UNIQUE (user_id, name)
-						  )";
-    $tables['concert'] = "CREATE TABLE IF NOT EXISTS concert(
-						  concert_id SERIAL ,
-						  artist_id INT NOT NULL , 
-						  date DATE NOT NULL , 
-						  city VARCHAR(30) NOT NULL , 
-						  attend BOOLEAN NOT NULL DEFAULT FALSE,
-						  notes VARCHAR(500) ,
-						  PRIMARY KEY (concert_id) ,
-						  FOREIGN KEY (artist_id) REFERENCES artist(artist_id),
-						  UNIQUE (artist_id, date)
-						  )";
-    $tables['token']   = "CREATE TABLE IF NOT EXISTS auth_token(
-                          token_id SERIAL ,
-                          selector CHAR(12) UNIQUE NOT NULL ,
-                          token CHAR(64) NOT NULL ,
-                          user_id INT NOT NULL ,
-                          expires TIMESTAMP NOT NULL ,
-                          PRIMARY KEY (token_id) ,
-                          FOREIGN KEY (user_id) REFERENCES users(user_id)
-                          )";
+    $tables['user']            =
+        "CREATE TABLE IF NOT EXISTS users(
+        user_id SERIAL,
+        email VARCHAR(30) UNIQUE NOT NULL ,
+        passwd VARCHAR(255) NOT NULL ,
+        name VARCHAR(50) NOT NULL ,
+        PRIMARY KEY (user_id))";
+    $tables['artist']          =
+        "CREATE TABLE IF NOT EXISTS artist(
+        artist_id SERIAL ,
+        user_id INT NOT NULL ,
+        name VARCHAR(50) NOT NULL ,
+        genre VARCHAR(50) NULL , 
+        country VARCHAR(50) NULL ,
+        hidden BOOLEAN NOT NULL DEFAULT FALSE ,
+        PRIMARY KEY (artist_id),
+        FOREIGN KEY (user_id) REFERENCES users(user_id),
+        UNIQUE (user_id, name))";
+    $tables['concert']         =
+        "CREATE TABLE IF NOT EXISTS concert(
+        concert_id SERIAL ,
+        date DATE NOT NULL , 
+        city VARCHAR(30) NOT NULL , 
+        venue VARCHAR(30) NULL ,
+        attend BOOLEAN NOT NULL DEFAULT FALSE,
+        notes VARCHAR(500) ,
+        PRIMARY KEY (concert_id));";
+    $tables['concert_artists'] =
+        "CREATE TABLE IF NOT EXISTS concert_artists(
+        id SERIAL ,
+        artist_id INT NOT NULL ,
+        concert_id INT NOT NULL , 
+        is_primary BOOLEAN NOT NULL DEFAULT FALSE,
+        PRIMARY KEY (id) ,
+        FOREIGN KEY (artist_id) REFERENCES artist(artist_id) ,
+        FOREIGN KEY (concert_id) REFERENCES concert(concert_id))";
+    $tables['token']           =
+        "CREATE TABLE IF NOT EXISTS auth_token(
+        token_id SERIAL ,
+        selector CHAR(12) UNIQUE NOT NULL ,
+        token CHAR(64) NOT NULL ,
+        user_id INT NOT NULL ,
+        expires TIMESTAMP NOT NULL ,
+        PRIMARY KEY (token_id) ,
+        FOREIGN KEY (user_id) REFERENCES users(user_id))";
 
     return $tables;
 }
@@ -153,7 +162,7 @@ function pgsql_tables()
  */
 function check_install()
 {
-    if (!file_exists('config.php')) {
+    if ( ! file_exists('config.php')) {
         die("Please run the <a href='install.php'>install script</a> set up Concert Tracker.");
     }
 }
@@ -194,7 +203,7 @@ function cookie_loader($dbh)
         $result = $stmt->fetch();
 
         if ($result != false) {
-            if (hash_equals($result['token'], hash("sha256", $token)) 
+            if (hash_equals($result['token'], hash("sha256", $token))
                 && strtotime($result['expires']) >= time()
             ) {
                 $_SESSION['user']     = $result['user_id'];
