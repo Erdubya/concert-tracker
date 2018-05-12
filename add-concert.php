86f737e5c6488dc6ceb33cb73af0f086295aeafd
Erik Wilson <erikwilson@outlook.com>
2018-05-12 15:04:37 -0400

    implement adding concerts

--- add-concert.php
+++ add-concert.php
@@ -12,16 +12,21 @@ require_once "_functions.php";
 session_start();
 $dbh = db_connect() or die(ERR_MSG);
 
-if (isset($_POST['submit'])) {
-    $stmt = $dbh->prepare("INSERT INTO concert(artist_id, date, city, attend) VALUES (:artist, :showdate, :city, :attend)");
-    $stmt->bindParam(':artist', $artist);
+//var_dump($_POST);
+//die();
+
+if (isset($_POST['add'])) {
+    $stmt = $dbh->prepare("INSERT INTO concert(date, city, attend, notes) VALUES (:showdate, :city, :attend, :notes)");
+//    $stmt->bindParam(':artist', $artist);
     $stmt->bindParam(':showdate', $date);
     $stmt->bindParam(':city', $city);
     $stmt->bindParam(':attend', $attend);
+    $stmt->bindParam(':notes', $notes);
 
-    $artist = $_POST['artist_id'];
+//    $artist = $_POST['artist_id'];
     $date   = $_POST['date'];
     $city   = $_POST['city'];
+    $notes  = $_POST['notes'];
     if (isset($_POST['attend'])) {
         $attend = 1;
     } else {
@@ -29,6 +34,38 @@ if (isset($_POST['submit'])) {
     }
 
     $stmt->execute();
+
+    $concertid = $dbh->lastInsertId();
+    add_new_artists($dbh, $concertid, $_POST['p_artist'], $_POST['o_artist']);
+}
+
+/**
+ * @param $dbh        PDO
+ * @param $concert_id int
+ * @param $p_artists  array
+ * @param $o_artists  array
+ */
+function add_new_artists($dbh, $concert_id, $p_artists, $o_artists)
+{
+    $stmt = $dbh->prepare("INSERT INTO concert_artists(artist_id, concert_id, is_primary) VALUES (:aid, :cid, :isp)");
+    $stmt->bindParam(':aid', $artist_id);
+    $stmt->bindParam(':cid', $concert_id);
+    $stmt->bindParam(':isp', $is_primary);
+
+    foreach ($p_artists as $a) {
+        /** @noinspection PhpUnusedLocalVariableInspection */
+        $artist_id  = $a;
+        /** @noinspection PhpUnusedLocalVariableInspection */
+        $is_primary = true;
+        $stmt->execute();
+    }
+    foreach ($o_artists as $a) {
+        /** @noinspection PhpUnusedLocalVariableInspection */
+        $artist_id  = $a;
+        /** @noinspection PhpUnusedLocalVariableInspection */
+        $is_primary = false;
+        $stmt->execute();
+    }
 }
 
 unset($_POST);
